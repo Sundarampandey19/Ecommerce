1510f24a94d867be7c279d67e4e9a48091fedfe8
sundaram <thisnewcomputerismine2020@gmail.com>
2024-09-06 20:22:44 +0530

    separated authOptions

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,24 @@
+import { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/lib/db/prisma";
+import { Adapter } from "next-auth/adapters";
+import GoogleProvider from "next-auth/providers/google";
+import { env } from "@/lib/env";
+
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma) as Adapter,
+    providers: [
+      GoogleProvider({
+        clientId: env.GOOGLE_CLIENT_ID,
+        clientSecret: env.GOOGLE_CLIENT_SECRET,
+      }),
+    ],
+    callbacks:{
+      session({session,user}){
+        session.user.id = user.id
+        return session
+  
+      }
+    }
+  };
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,27 +1,7 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import prisma from "@/lib/db/prisma";
-import { NextAuthOptions } from "next-auth";
-import { Adapter } from "next-auth/adapters";
-import GoogleProvider from "next-auth/providers/google";
 import NextAuth from "next-auth/next";
-import { env } from "@/lib/env";
+import { authOptions } from "./options";
 
- const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma) as Adapter,
-  providers: [
-    GoogleProvider({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  callbacks:{
-    session({session,user}){
-      session.user.id = user.id
-      return session
-
-    }
-  }
-};
+ 
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
